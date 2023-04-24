b7cc27c2258a5ffd174973706dbdc76d5585e51a
Omared250 <79874354+Omared250@users.noreply.github.com>
2023-04-24 14:42:18 +0200

    recovering data from a post

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 // public directory
 app.use( express.static('public') );
 
+// Read and Parse fo the body
+app.use( express.json() );
+
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 
