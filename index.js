e99af9a317c79fdc75b0cb51f4f98187eadf9037
Omared250 <79874354+Omared250@users.noreply.github.com>
2023-04-24 13:39:19 +0200

    setting up backend basis

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const express = require('express');
 
-console.log('Hello world!!!');
+// create express server
+const app = express();
+
+// Routes
+app.get('/', (req, res) => {
+    res.json({
+        ok: true
+    })
+});
+
+// listen petitions
+app.listen(4000, () => {
+    console.log(`Server running at port ${ 4000 }`);
+})
