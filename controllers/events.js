c280528fda31a7b27cc544279313d401928215b7
Omared250 <79874354+Omared250@users.noreply.github.com>
2023-04-25 11:29:47 +0200

    delete event

--- controllers/events.js
+++ controllers/events.js
@@ -46,7 +46,7 @@ const updateEvent = async( req, res = response ) => {
         const event = await Event.findById( eventId );
 
         if ( !event ) {
-            res.status(404).json({
+            return res.status(404).json({
                 ok: false,
                 msg: 'Event do not exist with that Id'
             });
@@ -80,13 +80,43 @@ const updateEvent = async( req, res = response ) => {
     }
 };
 
-const deleteEvent = ( req, res = response ) => {
+const deleteEvent = async( req, res = response ) => {
 
-    res.json({
-        ok: true,
-        msg: 'deleteEvent'
-    })
+    const eventId = req.params.id;
+    const uid = req.uid;
 
+    try {
+
+        const event = await Event.findById( eventId );
+
+        if ( !event ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Event do not exist with that Id'
+            });
+        }
+
+        if ( event.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No permissions to delete this event'
+            });
+        }
+
+        await Event.findByIdAndDelete( eventId );
+
+        res.json({
+            ok: true,
+            msg: 'Event deleted'
+        });
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            ok: false,
+            msg: 'Yous should contact admin'
+        });
+    }
 };
 
 module.exports = {
