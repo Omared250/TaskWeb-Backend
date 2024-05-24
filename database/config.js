b9c24e1d803d6deb86e6120988228b773d4dad88
Omared250 <79874354+Omared250@users.noreply.github.com>
2024-05-24 17:33:49 +0200

    refactoring db connection

--- database/config.js
+++ database/config.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
+const logger = require('../winston-config');
 
 const dbConnection = async () => {
   try {
     await mongoose.connect(process.env.DB_CNN);
     console.log('DB Online');
   } catch (error) {
-    console.error(`Error initializing DB: ${error}`);
+    logger.error('Error initializing DB', {
+        message: error.message,
+        stack: error.stack,
+        name: error.name
+    });
   }
 };
 
